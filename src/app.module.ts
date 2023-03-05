ceac5ee7c72591c13a763d96ab2c831147fcdbeb
Pampaka <pampakadev@yandex.ru>
2023-03-06 00:06:24 +0300

    add sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
 
-@Module({})
+@Module({
+	imports: [
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.DB_HOST || 'localhost',
+			port: Number(process.env.DB_PORT) || 5432,
+			username: process.env.DB_USER || 'mi-life',
+			password: process.env.DB_PASS || 'root',
+			database: process.env.DB_NAME || 'mi-life',
+			models: [],
+			autoLoadModels: true
+		})
+	]
+})
 export class AppModule {}
